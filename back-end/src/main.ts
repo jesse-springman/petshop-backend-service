5c500857b1b5925b9e9a96e69754c19f52b64902
jesse-springman <jessebarbosa45@gmail.com>
2026-01-27 19:47:19 -0300

    fix: add `trust proxy` for enable Cookie secure

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,9 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
 
   app.use(cookieParser());
 

1d78738c1069642b8e3d049e26cb05dfb81e89b5
jesse-springman <jessebarbosa45@gmail.com>
2026-02-11 20:03:28 -0300

    feat: create use-cases `GET` and `POST` for endpoind `/agenda`

---
+++ back-end/src/use-cases/get-agenda.ts
@@ -0,0 +1,40 @@
+import { PrismaService } from '../prisma/database/prisma.service';
+import { Injectable, BadRequestException } from '@nestjs/common';
+
+interface dtoAppointments {
+  start: string;
+  end: string;
+}
+
+Injectable();
+export class GetAgenda {
+  constructor(private prisma: PrismaService) {}
+
+  async execute({ start, end }: dtoAppointments) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+      throw new BadRequestException('Intervalo de datas inválido');
+    }
+
+    const appointments = await this.prisma.appointment.findMany({
+      where: {
+        date: {
+          gte: startDate,
+          lt: endDate,
+        },
+
+        status: 'SCHEDULED',
+      },
+      include: {
+        customer: true,
+      },
+      orderBy: {
+        date: 'asc',
+      },
+    });
+
+    return appointments;
+  }
+}
---
+++ back-end/src/use-cases/post-agenda.ts
@@ -0,0 +1,54 @@
+import { PrismaService } from '../prisma/database/prisma.service';
+import { Prisma } from '@prisma/client';
+
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
+
+interface dtoTime {
+  customerId: string;
+  date: string;
+  notes?: string;
+}
+
+@Injectable()
+export class CreateAppointment {
+  constructor(private prisma: PrismaService) {}
+
+  async execute(data: dtoTime) {
+    const parsedDate = new Date(data.date);
+
+    if (isNaN(parsedDate.getTime())) {
+      throw new BadRequestException('Data inválida');
+    }
+
+    const customer = await this.prisma.customer.findUnique({
+      where: { id: data.customerId },
+    });
+
+    if (!customer) {
+      throw new NotFoundException('Cliente não encontrado');
+    }
+
+    try {
+      const appointment = await this.prisma.appointment.create({
+        data: {
+          customerId: data.customerId,
+          date: parsedDate,
+          notes: data.notes,
+        },
+      });
+      return appointment;
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      )
+        throw new ConflictException('Já existe um agendamento nesse horário');
+      throw error;
+    }
+  }
+}

56b209f78fb10abbd0559a6c1015132931828667
jesse-springman <jessebarbosa45@gmail.com>
2026-04-27 20:05:37 -0300

    docs: add new endpoint `/respostaIA` in documentation

--- back-end/src/messageAI/ai.controller.ts
+++ back-end/src/messageAI/ai.controller.ts
@@ -1,14 +1,43 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { GenerateMessage } from './use-cases/post-generate-message';
 import { GenerarteMessageDto } from './dto/create-message.dto';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiResponse,
+  ApiQuery,
+  ApiOperation,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @UseGuards(AuthGuard)
+@ApiTags('ai')
 @Controller('ai')
 export class aiController {
   constructor(private readonly generateMessage: GenerateMessage) {}
 
   @Post('generate-message')
+  @ApiOperation({
+    summary: 'Cria mensagem personalizada com IA',
+    description:
+      'Gera uma mensagem automática para o cliente com base no tipo selecionado (lembrete, agendamento ou cobrança).',
+  })
+  @ApiBody({ type: GenerarteMessageDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Mensagem criada com sucesso',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Esse usuário não foi encontrado',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Token inválido ou não enviado',
+  })
   async responseIA(@Body() body: GenerarteMessageDto) {
     const result = await this.generateMessage.execute(body);
     return result;

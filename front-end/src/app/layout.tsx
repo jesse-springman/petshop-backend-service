bfc7d60dede6cc1eaafda7ab8490e748d47ad85c
jesse-springman <jessebarbosa45@gmail.com>
2026-05-06 19:20:07 -0300

    refactor: add favicon

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,9 +1,8 @@
-"use client";
 import "./globals.css";
 import { UserProvider } from "@/context/UserContext";
 import { Toaster } from "react-hot-toast";
-
 import { JetBrains_Mono, Outfit } from "next/font/google";
+import type { Metadata } from "next";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -14,6 +13,13 @@ const outfit = Outfit({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "New-Pettz",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐶</text></svg>",
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
